43ee255f0b3a62be88ab3d8c7b900f0752cd5e05
JkyProgrammer <32058792+JkyProgrammer@users.noreply.github.com>
2021-01-07 16:18:38 +0000

    Create caesarcipher.py

---
+++ caesarcipher.py
@@ -0,0 +1,12 @@
+
+plaintext = input ("Enter some plaintext (spaces will not be preserved): ").lower()
+while True:
+    key = int(input ("Enter a key: ")) % 26
+    ciphertext = ""
+    for c in plaintext:
+        if c != ' ':
+            tmp = ord(c)+key
+            if (tmp > ord('z')):
+                tmp -= 26
+            ciphertext += chr(tmp)
+    print (plaintext + " -> " + ciphertext)
